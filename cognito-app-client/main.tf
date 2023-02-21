ca00b366138fdf711849ef97bfb9e5f5198bed09
halvor mangseth <h.mangseth@gmail.com>
2023-02-21 10:31:03 +0100

    Let the applications themselves handle the ssm parameters.

--- cognito-app-client/main.tf
+++ cognito-app-client/main.tf
@@ -2,10 +2,6 @@ data "aws_ssm_parameter" "user_pool" {
   name = "/digitalekanaler/common-services/microservices-auth/pool-id"
 }
 
-data "aws_kms_key" "application_key" {
-  key_id = var.kms_key
-}
-
 resource "aws_cognito_user_pool_client" "this" {
   name                          = var.app_name
   user_pool_id                  = data.aws_ssm_parameter.user_pool.value
@@ -18,23 +14,4 @@ resource "aws_cognito_user_pool_client" "this" {
 
   allowed_oauth_flows  = ["client_credentials"]
   allowed_oauth_scopes = var.oauth_scopes
-}
-
-resource "aws_ssm_parameter" "client_id" {
-  count = local.should_add_secret_to_secrets_manager
-  name  = "/config/${var.app_name}/oauth2/clientId"
-  type  = "String"
-  value = aws_cognito_user_pool_client.this.id
-}
-
-resource "aws_ssm_parameter" "client_secret" {
-  count = local.should_add_secret_to_secrets_manager
-  name  = "/config/${var.app_name}/oath2/clientSecret"
-  type  = "SecureString"
-  key_id = data.aws_kms_key.application_key.id
-  value = aws_cognito_user_pool_client.this.client_secret
-}
-
-locals {
-  should_add_secret_to_secrets_manager = var.store_credentials ? 1 : 0
 }
--- cognito-app-client/variables.tf
+++ cognito-app-client/variables.tf
@@ -1,15 +1,6 @@
 variable "app_name" {}
 
-variable "kms_key" {
-  type = string
-  description = "The identifier of the applications' kms key"
-}
-
 variable "oauth_scopes" {
   type    = list(string)
   default = []
-}
-
-variable "store_credentials" {
-  default = true
 }
