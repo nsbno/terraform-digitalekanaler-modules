18d3ec2923f9bc2e35bd1672cc3d1155437cd9be
Petter Daae <petter.daae@bekk.no>
2023-09-11 09:40:30 +0200

    Create common spring boot service module

---
+++ spring-boot-service/external.tf
@@ -0,0 +1,20 @@
+data "aws_ssm_parameter" "shared_config" {
+  name = "/digitalekanaler/shared-config"
+}
+
+data "aws_ssm_parameter" "datadog_apikey" {
+  name = "/external/datadog/apikey"
+}
+
+data "aws_ssm_parameter" "log_router_image" {
+  name = "/aws/service/aws-for-fluent-bit/stable"
+}
+
+data "aws_lb" "internal_lb" {
+  arn = local.shared_config.lb_internal_arn
+}
+
+data "aws_route53_zone" "internal_vydev_io_zone" {
+  name         = local.shared_config.internal_hosted_zone_name
+  private_zone = true
+}
---
+++ spring-boot-service/outputs.tf
@@ -0,0 +1,19 @@
+output "task_role_name" {
+  value = module.task.task_role_name
+}
+
+output "security_group_id" {
+  value = module.task.security_group_id
+}
+
+output "task_execution_role_name" {
+  value = module.task.task_execution_role_name
+}
+
+output "application_key_id" {
+  value = aws_kms_key.application_key.id
+}
+
+output "application_key_arn" {
+  value = aws_kms_key.application_key.arn
+}
