fc923afe7540b8e3f829e4aef656914acccc52df
faergestad <georg.f.persen@gmail.com>
2024-03-06 16:01:56 +0100

    Change internal AppRunner module to be created from scratch

--- apprunner-internal/main.tf
+++ apprunner-internal/main.tf
@@ -1,56 +1,218 @@
-module "apprunner" {
-  source      = "github.com/nsbno/terraform-aws-apprunner-service?ref=1.0.0"
-  name_prefix = "${var.name_prefix}-${var.application_name}"
+locals {
+  use_vpc_connector = (length(var.vpc_config.subnet_ids) > 0 && length(var.vpc_config.security_groups) > 0)
+}
 
-  auto_deployment  = true
-  application_port = "${var.app_port}"
 
-  vpc_config = {
-    subnet_ids      = toset(data.aws_subnets.private.id)
-    security_groups = [aws_security_group.apprunner_security_group.id]
+##################################
+#                                #
+# AppRunner service              #
+#                                #
+##################################
+
+resource "aws_apprunner_service" "service" {
+  service_name = var.name_prefix
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.ecr_access_role.arn
+    }
+    image_repository {
+      image_identifier      = "${aws_ecr_repository.ecr.repository_url}:${var.image_tag}"
+      image_repository_type = "ECR"
+      image_configuration {
+        port                          = var.application_port
+        runtime_environment_variables = var.environment_variables
+        runtime_environment_secrets   = var.environment_secrets
+      }
+    }
+    auto_deployments_enabled = var.auto_deployment
   }
 
-  environment_variables = var.environment_variables
+  instance_configuration {
+    cpu               = var.cpu
+    memory            = var.memory
+    instance_role_arn = aws_iam_role.task_role.arn
+  }
 
-  environment_secrets = var.environment_secrets
+  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.autoscaling.arn
 
-  ecr_arn = data.aws_ecr_repository.ecr.arn
-  ecr_url = data.aws_ecr_repository.ecr.repository_url
+  dynamic "network_configuration" {
+    for_each = local.use_vpc_connector ? aws_apprunner_vpc_connector.service : []
 
-  domain_name = {
-    name = "${var.application_name}.vylabs.io"
-    zone = "vylabs.io"
+    content {
+      egress_configuration {
+        egress_type       = "VPC"
+        vpc_connector_arn = try(network_configuration.value.arn, null)
+      }
+    }
+  }
+
+  tags = var.tags
+
+  depends_on = [
+    aws_iam_role.ecr_access_role
+  ]
+}
+
+resource "aws_apprunner_vpc_connector" "service" {
+  count              = local.use_vpc_connector ? 1 : 0
+  vpc_connector_name = var.name_prefix
+  subnets            = var.vpc_config.subnet_ids
+  security_groups    = var.vpc_config.security_groups
+
+  tags = var.tags
+}
+
+resource "aws_apprunner_auto_scaling_configuration_version" "autoscaling" {
+  auto_scaling_configuration_name = "limited-scaling"
+  max_concurrency                 = var.auto_scaling.max_concurrency
+  min_size                        = var.auto_scaling.min_instances
+  max_size                        = var.auto_scaling.max_instances
+
+  tags = var.tags
+}
+
+##################################
+#                                #
+# ECR                            #
+#                                #
+##################################
+resource "aws_ecr_repository" "ecr" {
+  name = "${var.name_prefix}-internal-${var.application_name}"
+}
+
+resource "aws_ecr_lifecycle_policy" "keep_last_100_images" {
+  repository = aws_ecr_repository.ecr.name
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep last N images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 100
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+##################################
+#                                #
+# Task role                      #
+#                                #
+##################################
+
+resource "aws_iam_role" "task_role" {
+  name               = "${var.name_prefix}-task-role"
+  assume_role_policy = data.aws_iam_policy_document.task_assume_policy.json
+
+  tags = var.tags
+}
+
+data "aws_iam_policy_document" "task_assume_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["tasks.apprunner.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
   }
 }
 
-data "aws_vpc" "selected" {
-  id = var.vpc_id
+
+##################################
+#                                #
+# Access role                    #
+#                                #
+##################################
+
+resource "aws_iam_role" "ecr_access_role" {
+  name               = "${var.name_prefix}-access-role"
+  assume_role_policy = data.aws_iam_policy_document.access_assume_policy.json
+
+  tags = var.tags
 }
 
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
+data "aws_iam_policy_document" "access_assume_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["build.apprunner.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
   }
+}
 
-  tags = {
-    Tier = "Private" # TODO dette må endres til å matche indetifiserende tag i det nye kontosettet
+data "aws_iam_policy_document" "access_policy" {
+  statement {
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:DescribeImages",
+    ]
+    resources = [aws_ecr_repository.ecr.arn]
+  }
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
   }
 }
 
-data "aws_ecr_repository" "ecr" {
-  name = "${var.name_prefix}-${var.application_name}"
+resource "aws_iam_policy" "access_policy" {
+  name   = "${var.name_prefix}-access-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.access_policy.json
 }
 
-resource "aws_security_group" "apprunner_security_group" {
-  vpc_id = var.vpc_id
+resource "aws_iam_role_policy_attachment" "access_role_policy_attachment" {
+  role       = aws_iam_role.ecr_access_role.name
+  policy_arn = aws_iam_policy.access_policy.arn
 }
 
-resource "aws_security_group_rule" "allow_all_outgoing_traffic_from_apprunner" {
-  security_group_id = aws_security_group.apprunner_security_group.id
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+##################################
+#                                #
+# Custom domain name             #
+#                                #
+##################################
+
+data "aws_route53_zone" "zone" {
+  name = var.domain_name.zone
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = var.domain_name.name
+  type    = "CNAME"
+  ttl     = 3600
+  records = [aws_apprunner_service.service.service_url]
+}
+
+resource "aws_apprunner_custom_domain_association" "service" {
+  domain_name          = aws_route53_record.record.name
+  service_arn          = aws_apprunner_service.service.arn
+  enable_www_subdomain = false
+}
+
+resource "aws_route53_record" "validation" {
+  name = aws_apprunner_custom_domain_association.service.certificate_validation_records[0].name
+  records = [
+    aws_apprunner_custom_domain_association.service.certificate_validation_records[0]
+  ]
+  ttl     = 3600
+  type    = aws_apprunner_custom_domain_association.service.certificate_validation_records[0].type
+  zone_id = data.aws_route53_zone.zone.zone_id
+
+  depends_on = [
+    aws_apprunner_custom_domain_association.service
+  ]
 }
--- apprunner-internal/variables.tf
+++ apprunner-internal/variables.tf
@@ -1,16 +1,48 @@
 variable "name_prefix" {
-  type    = string
-  default = "digitalekanaler"
+  description = "A prefix used for naming resources."
+  type        = string
 }
 
 variable "application_name" {
-  type        = string
-  description = "A short and lowercase name for the service (examples: ticket, booking, smartpris). Must be unique to the service."
+  description = "The name of the application"
+  type = string
 }
 
-variable "app_port" {
+variable "tags" {
+  description = "A map of tags (key-value pairs) passed to resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "cpu" {
+  description = "The number of CPU units available to the application (1024|2048)"
+  type        = string
+  default     = "1024"
+}
+
+variable "memory" {
+  description = "The amount of memory in MB available to the application (2048|3072|4096)"
+  type        = string
+  default     = "2048"
+}
+
+variable "auto_scaling" {
+  description = "Autoscaling configuration"
+  type = object({
+    min_instances   = number
+    max_instances   = number
+    max_concurrency = number
+  })
+  default = {
+    min_instances   = 1
+    max_instances   = 2
+    max_concurrency = 100
+  }
+}
+
+variable "application_port" {
+  description = "The application port number"
   type        = number
-  description = "The port the application is running on"
 }
 
 variable "environment_variables" {
@@ -25,4 +57,34 @@ variable "environment_secrets" {
   default     = {}
 }
 
-variable "vpc_id" {}
+variable "image_tag" {
+  description = "The tag of the application image to deploy"
+  type        = string
+  default     = "latest"
+}
+
+variable "auto_deployment" {
+  description = "Automatically deploy when a new image version is pushed to ECR"
+  type        = bool
+  default     = false
+}
+
+variable "domain_name" {
+  description = "A map containing a domain and name of the associated hosted zone."
+  type = object({
+    name = string
+    zone = string
+  })
+}
+
+variable "vpc_config" {
+  description = "Enable outbound VPC access for service"
+  type = object({
+    subnet_ids      = list(string)
+    security_groups = list(string)
+  })
+  default = {
+    subnet_ids      = []
+    security_groups = []
+  }
+}
