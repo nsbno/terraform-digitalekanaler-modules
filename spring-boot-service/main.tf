755ac092e89861f9a4810d7ba4397933b2213867
Mohamad Kenas <mohamad.kenas@vy.no>
2025-10-06 14:10:26 +0200

    Update spring-boot-service/main.tf
    Co-authored-by: Wictor Zhao <wictorrr@gmail.com>

--- spring-boot-service/main.tf
+++ spring-boot-service/main.tf
@@ -27,7 +27,7 @@ resource "terraform_data" "no_spot_in_prod" {
 }
 
 module "task" {
-  source = "github.com/nsbno/terraform-aws-ecs-service?ref=26e271a"
+  source = "github.com/nsbno/terraform-aws-ecs-service?ref=3.0.0-rc7"
   depends_on         = [terraform_data.no_spot_in_prod]
   service_name       = local.name_with_prefix
   vpc_id             = local.shared_config.vpc_id
