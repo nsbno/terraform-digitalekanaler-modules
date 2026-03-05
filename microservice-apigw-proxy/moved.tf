9415a790c18c22f33349b09a88fa42eb9da8e169
mKenas <mohamad.kenas@vy.no>
2026-03-05 08:42:01 +0100

    Revert "Add moved blocks to avoid change old api resources after adding support for removing the old http api"
    This reverts commit 89dfee8dc89be9fa65fd101a6c196290fc77c412.

--- microservice-apigw-proxy/moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = aws_apigatewayv2_route.this
-  to   = aws_apigatewayv2_route.this[0]
-}
-
-moved {
-  from = aws_apigatewayv2_integration.this
-  to   = aws_apigatewayv2_integration.this[0]
-}
