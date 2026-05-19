3a583875c778f259ed6e46bd93c730b17a459f69
erlendgjelsvik <erlendgje@gmail.com>
2026-05-19 15:23:26 +0200

    test this

--- cognito-app-client/variables.tf
+++ cognito-app-client/variables.tf
@@ -13,11 +13,11 @@ variable "refresh_token_validity" {
 
 variable "token_validity_units" {
   description = "Time units for token validity."
-  type = object({
+  type = optional(object({
     access_token  = optional(string, "hours")
     id_token      = optional(string, "hours")
     refresh_token = optional(string, "days")
-  })
+  }), object({}))
 
   # Validates that any provided values exactly match the allowed strings
   validation {
