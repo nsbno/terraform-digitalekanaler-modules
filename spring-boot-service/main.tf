e5b450725d1cf565373ba62f737f57c53dc5e91a
Raees Abbas <raees.abbas@vy.no>
2026-06-19 15:27:02 +0200

    Update ECS service module source reference to use CPU and Memory reservation

--- spring-boot-service/main.tf
+++ spring-boot-service/main.tf
@@ -27,7 +27,7 @@ resource "terraform_data" "no_spot_in_prod" {
 }
 
 module "task" {
-  source                          = "github.com/nsbno/terraform-aws-ecs-service?ref=3.1.0"
+  source                          = "github.com/nsbno/terraform-aws-ecs-service?ref=reservation-cpu-memory"
   depends_on                      = [terraform_data.no_spot_in_prod]
   service_name                    = local.name_with_prefix
   vpc_id                          = local.shared_config.vpc_id
