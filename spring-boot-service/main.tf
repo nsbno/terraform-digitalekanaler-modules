4bfa28f6fd0e235f5bb70b810141ec65ef40a06f
Raees Abbas <raees.abbas@vy.no>
2026-06-22 14:08:48 +0200

    Added datadog_options.

--- spring-boot-service/main.tf
+++ spring-boot-service/main.tf
@@ -40,6 +40,7 @@ module "task" {
   datadog_instrumentation_runtime = "jvm" # Can be jvm or node
   team_name_override              = var.datadog_team_name
   dd_service_name_override        = var.datadog_service_name
+  datadog_options                 = var.datadog_options
   datadog_environment_variables = {
     DD_DOGSTATSD_NON_LOCAL_TRAFFIC = "true"
     DD_CHECKS_TAG_CARDINALITY      = "orchestrator"
