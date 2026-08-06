ae108318f7e2b503bfcd191d1b71e960aa7c6abd
Tom Arne <tom.brandvold@vy.no>
2026-08-06 12:12:56 +0200

    Bump ECS service module version to 3.5.0 and add is_preview_supported variable

--- spring-boot-service/main.tf
+++ spring-boot-service/main.tf
@@ -27,7 +27,7 @@ resource "terraform_data" "no_spot_in_prod" {
 }
 
 module "task" {
-  source                          = "github.com/nsbno/terraform-aws-ecs-service?ref=3.4.1"
+  source                          = "github.com/nsbno/terraform-aws-ecs-service?ref=3.5.0"
   depends_on                      = [terraform_data.no_spot_in_prod]
   service_name                    = local.name_with_prefix
   vpc_id                          = local.shared_config.vpc_id
@@ -135,6 +135,8 @@ module "task" {
   propagate_tags = "TASK_DEFINITION"
 
   lb_stickiness = var.lb_stickiness
+
+  is_preview_supported = var.is_preview_supported
 }
 
 #########################################
